350ab6245f9a56082e5c1b6bfaf8fd557ea0ff97
unknown <vikram.optum@gmail.com>
2020-11-07 17:30:26 +0530

    <In progress changes>

--- src/main/java/com/vs/service/SmsService.java
+++ src/main/java/com/vs/service/SmsService.java
@@ -16,14 +16,14 @@ import com.twilio.rest.api.v2010.account.Message;
 @Service
 public class SmsService {
     // Find your Account Sid and Token at twilio.com/console
-    public static final String ACCOUNT_SID = "AC2f6b38fcad89e9ab1916bee8d159bd99";
-    public static final String AUTH_TOKEN = "4dd694ca06b559305b5423335e235d8f";
+    public static final String ACCOUNT_SID = "XXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "XXXXXXXXXXXXXXXXXXXX";
 
     public Message sendSMS() {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                new com.twilio.type.PhoneNumber("+917696250550"),//The phone number you are sending text to
-                new com.twilio.type.PhoneNumber("+12059227798"),//The Twilio phone number
+                new com.twilio.type.PhoneNumber("+XXXXXXXXXXX"),//The phone number you are sending text to
+                new com.twilio.type.PhoneNumber("+XXXXXXXXXXX"),//The Twilio phone number
                 "Teaching is The new learning")
            .create();
 
